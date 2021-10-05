0fc3d4c7646306972f5d329a9015bfbce5609d41
Ankur Pande <ankur.pande108@gmail.com>
2021-10-05 12:04:25 +0530

    Initial Commit

---
+++ src/main/java/snl/GreenSnake.java
@@ -0,0 +1,16 @@
+package snl;
+
+public class GreenSnake extends Snake {
+
+
+    public GreenSnake(int startPosition, int endPosition) {
+        super(startPosition, endPosition);
+    }
+
+
+    @Override
+    public void biteDecision(Snake snake) {
+
+        super.biteDecision(snake);
+    }
+}
