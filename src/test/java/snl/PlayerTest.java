ced61e43087310469e5b1ee40b954ea7a96ad322
Ankur Pande <ankur.pande108@gmail.com>
2021-10-05 12:09:01 +0530

    Initial Commit

---
+++ src/test/java/snl/PlayerTest.java
@@ -0,0 +1,19 @@
+package snl;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+    @Test
+    public void shouldMoveToNextPositionBasedOnDiceRoll() {
+        Player player = new Player();
+        assertEquals(0, player.getCurrentPosition());
+
+        //when
+        player.move(10);
+        //then
+        assertEquals(10, player.getCurrentPosition());
+    }
+}
