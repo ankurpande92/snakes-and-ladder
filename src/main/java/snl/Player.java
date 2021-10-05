05accc2512ff54ff42958c86ad5e3c594022dc5c
Ankur Pande <ankur.pande108@gmail.com>
2021-10-05 12:06:39 +0530

    Initial Commit

---
+++ src/main/java/snl/Player.java
@@ -0,0 +1,20 @@
+package snl;
+
+public class Player {
+    private int currentPosition = 0;
+
+    public Player() {
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public void move(int newPosition) {
+        setCurrentPosition(getCurrentPosition() + newPosition);
+    }
+}
