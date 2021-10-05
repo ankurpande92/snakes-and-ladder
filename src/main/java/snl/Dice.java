4a15119ef2d1bfb64fd0098f92f263f649ffdb20
Ankur Pande <ankur.pande108@gmail.com>
2021-10-05 12:05:18 +0530

    Initial Commit

---
+++ src/main/java/snl/Dice.java
@@ -0,0 +1,24 @@
+package snl;
+
+import java.util.Random;
+
+public class Dice {
+    private Random random;
+
+    public Dice() {
+        random = new Random();
+    }
+
+    public Dice(Random random) {
+        this.random = random;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public int roll() {
+        int diceNumber = random.nextInt(7);
+        return diceNumber == 0 ? 1 : diceNumber;
+    }
+}
